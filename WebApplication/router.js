16c0290b45960232c85d21a909d74dbbef2f0016
bondkyal <bondkyal@ecs.vuw.ac.nz>
2017-08-03 16:36:39 +1200

    Updated node.js server to use socket.io

--- WebApplication/router.js
+++ WebApplication/router.js
@@ -2,9 +2,10 @@ var express = require('express')
 var router = express.Router()
 
 router.get('/', function(req, res) {
-  res.render('index', {
-    title: 'Home'
-  })
+  //res.render('index', {
+    //title: 'Home'
+  //})
+  res.send('hello world')
 })
 
 router.get('/login', function(req, res) {
@@ -12,3 +13,5 @@ router.get('/login', function(req, res) {
     title: 'About'
   })
 })
+
+module.exports = router;
