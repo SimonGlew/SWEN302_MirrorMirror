14262369d0bfd01d7e01fa4be3203dd48d208b7e
hayandr1 <hayandr1@ecs.vuw.ac.nz>
2017-08-24 16:45:55 +1200

    NavigationView Drawer template implemented. Refactored main image and image history into new layout xml 'content_main'. Pair programmed with Jack

--- AndroidApplication/MirrorMirrorAndroid/app/src/main/java/mirrormirror/swen302/mirrormirrorandroid/activities/HomeActivity.java
+++ AndroidApplication/MirrorMirrorAndroid/app/src/main/java/mirrormirror/swen302/mirrormirrorandroid/activities/HomeActivity.java
@@ -1,14 +1,22 @@
 package mirrormirror.swen302.mirrormirrorandroid.activities;
 
+import android.app.Fragment;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.media.Image;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -19,9 +27,12 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.io.File;
@@ -42,8 +53,11 @@ import mirrormirror.swen302.mirrormirrorandroid.utilities.ServerController;
  * Created by bondkyal on 10/08/17.
  */
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
+    private DrawerLayout drawerLayout;
+    private NavigationView navView;
+    private ActionBarDrawerToggle drawerToggle;
 
     public static final int CAMERA_ACTIVITY_REQUEST_CODE = 10;
 
@@ -56,17 +70,34 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_activity);
-//        setImageClickListeners();
-//        setButtonClickListeners();
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        navView = (NavigationView) findViewById(R.id.nav_view);
+        navView.setNavigationItemSelectedListener(this);
+        ActionBar titleBar = getSupportActionBar();
+        titleBar.setDisplayHomeAsUpEnabled(true);
+        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.drawer_open, R.string.drawer_close);
+        drawerLayout.addDrawerListener(drawerToggle);
+
         initialLoadImages();
         setRecyclerView();
+    }
 
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        drawerToggle.syncState();
+    }
 
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        drawerToggle.onConfigurationChanged(newConfig);
     }
 
 
     private void setRecyclerView(){
-
         recyclerView = (RecyclerView)findViewById(R.id.horizontal_recycler_view);
         horizontalAdapter = new HorizontalAdapter(filePaths,this);
         LinearLayoutManager horizontalLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
@@ -74,32 +105,6 @@ public class HomeActivity extends AppCompatActivity {
         recyclerView.setAdapter(horizontalAdapter);
     }
 
-    private void setButtonClickListeners(){
-//        Button weightSubmit = (Button) findViewById(R.id.submit_weight);
-        //final EditText weightField = (EditText)findViewById(R.id.weight_text_field);
-
-//        weightSubmit.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Float weight = Float.valueOf(weightField.getText().toString());
-//                ServerController.sendWeight(weight, DateTimeManager.getDatetimeAsString(), getApplicationContext());
-//                weightField.clearFocus();
-//                hideKeyboard();
-//            }
-//        });
-//        weightField.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-//            @Override
-//            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-//                if(actionId == EditorInfo.IME_ACTION_DONE){
-//                    Float weight = Float.valueOf(weightField.getText().toString());
-//                    ServerController.sendWeight(weight, DateTimeManager.getDatetimeAsString(), getApplicationContext());
-//                    weightField.clearFocus();
-//                    hideKeyboard();
-//                }
-//                return false;
-//            }
-//        });
-    }
 
     private void hideKeyboard(){
         View view = this.getCurrentFocus();
@@ -121,7 +126,8 @@ public class HomeActivity extends AppCompatActivity {
         if(item.getItemId() == R.id.take_image){
             Intent intent = new Intent(this, CameraPreviewActivity.class);
             startActivityForResult(intent, CAMERA_ACTIVITY_REQUEST_CODE);
-
+        } else if (drawerToggle.onOptionsItemSelected(item)) {
+            return true;
         }
         return super.onOptionsItemSelected(item);
     }
@@ -147,4 +153,13 @@ public class HomeActivity extends AppCompatActivity {
         }
         recyclerView.getAdapter().notifyDataSetChanged();
     }
+
+    //React to items selected within the sidebar.
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        //if(id == R.id.drawer_placeholder1)...
+        return true;
+    }
+
 }
