86c34acf18c160d6fea5bbdc778bc1bbd18ad38f
hayandr1 <hayandr1@ecs.vuw.ac.nz>
2017-08-29 14:23:13 +1200

    Pushing missing build files

--- AndroidApplication/MirrorMirrorAndroid/app/src/main/java/mirrormirror/swen302/mirrormirrorandroid/utilities/InputWeightDialog.java
+++ AndroidApplication/MirrorMirrorAndroid/app/src/main/java/mirrormirror/swen302/mirrormirrorandroid/utilities/InputWeightDialog.java
@@ -23,13 +23,13 @@ import mirrormirror.swen302.mirrormirrorandroid.R;
  * Created by hayandr1 on 29/08/17.
  */
 
-public class InputWeightDialog extends AlertDialog implements View.OnClickListener, Spinner.OnItemSelectedListener {
+public class InputWeightDialog extends AlertDialog implements View.OnClickListener{
     private static final int maxWeight = 200;
     private static final double subKGStepping = 0.1;
     private static final DecimalFormat dF = new DecimalFormat(".##");
 
-    private static float leftSelected;
-    private static float rightSelected;
+    private static NumberPicker leftPicker;
+    private static NumberPicker rightPicker;
 
     public InputWeightDialog(Context context){
         super(context);
@@ -44,10 +44,10 @@ public class InputWeightDialog extends AlertDialog implements View.OnClickListen
         for(int n = 0; n < (int)(1/subKGStepping); n++){
             rightAdapterVals[n] = dF.format(subKGStepping*n);
         }
-        NumberPicker leftPicker = (NumberPicker) findViewById(R.id.weight_picker_left);
-        NumberPicker rightPicker = (NumberPicker) findViewById(R.id.weight_picker_right);
-        leftPicker.setDisplayedValues(leftAdapterVals);
-        rightPicker.setDisplayedValues(rightAdapterVals);
+        leftPicker = (NumberPicker) findViewById(R.id.weight_picker_left);
+        rightPicker = (NumberPicker) findViewById(R.id.weight_picker_right);
+        //leftPicker.setDisplayedValues(leftAdapterVals);
+        //rightPicker.setDisplayedValues(rightAdapterVals);
     }
 
     @Override
@@ -55,7 +55,7 @@ public class InputWeightDialog extends AlertDialog implements View.OnClickListen
         super.onCreate(savedInstanceState);
         setContentView(R.layout.input_weight_dialog);
 
-        populateSpinnerAdapters();
+        //populateSpinnerAdapters();
         Button cancel = (Button) findViewById(R.id.cancel_button);
         Button submit = (Button) findViewById(R.id.submit_button);
         cancel.setOnClickListener(this);
@@ -67,24 +67,25 @@ public class InputWeightDialog extends AlertDialog implements View.OnClickListen
         if(v.getId() == R.id.cancel_button){
             cancel();
         } else if(v.getId() == R.id.submit_button){
-            ServerController.sendWeight(leftSelected+rightSelected, DateTimeManager.getDatetimeAsString(), getContext());
+//            int test = leftPicker.getValue()+rightPicker.getValue();
+//            ServerController.sendWeight(test, DateTimeManager.getDatetimeAsString(), getContext());
             cancel();
         }
     }
 
 
-    @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        String selected = (String) parent.getItemAtPosition(position);
-//        if(parent.getId() == R.id.weight_spinner_left){
-//            leftSelected = Float.valueOf(selected);
-//        } else if(parent.getId() == R.id.weight_spinner_right){
-//            rightSelected = Float.valueOf(selected);
-//        }
-    }
-
-    @Override
-    public void onNothingSelected(AdapterView<?> parent) {
-
-    }
+//    @Override
+//    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+//        String selected = (String) parent.getItemAtPosition(position);
+////        if(parent.getId() == R.id.weight_spinner_left){
+////            leftSelected = Float.valueOf(selected);
+////        } else if(parent.getId() == R.id.weight_spinner_right){
+////            rightSelected = Float.valueOf(selected);
+////        }
+//    }
+//
+//    @Override
+//    public void onNothingSelected(AdapterView<?> parent) {
+//
+//    }
 }
